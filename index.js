3492fe71e35d9635ff69ee71baffa2ac34f9b0bf
gloria <pureainu@gmail.com>
2018-12-05 11:35:23 +0900

    [Add] script for utility

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // import fs from 'fs';
+// import map from './src/utils';
+const map = require('./src/utils');
 
-console.log('Hello, World');
+console.log(map([1, 2, 3, 4, 5], item => item + 10));
---
+++ src/utils.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @param {List, Array} list
+ * @param {Function} predicate
+ */
+function map(list, predicate) {
+  let result;
+
+  if (Array.isArray(list)) {
+    result = [];
+    const len = list.length;
+    for (let i = 0; i < len; i += 1) {
+      result[i] = predicate(list[i], i, list);
+    }
+  } else {
+    result = {};
+    const keys = Object.keys(list);
+    const len = keys.length;
+    for (let i = 0; i < len; i += 1) {
+      result[i] = predicate(list[keys[i]], keys[i], list);
+    }
+  }
+
+  return result;
+}
+
+export default map;
