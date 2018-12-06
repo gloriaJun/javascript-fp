968d7ebde09dad163cb9be30ea2bfad853508146
gloria <pureainu@gmail.com>
2018-12-06 17:06:00 +0900

    [Modify] webpack - babel configuration

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        }
+      }
+    ],
+  },
 };
