56a1072bb853ee86c7dc69984ab874fd08e7569a
gloriaJun <pureainu@gmail.com>
2019-07-24 01:12:13 +0900

    fix: Fix the test error of map

--- src/object/map.spec.js
+++ src/object/map.spec.js
@@ -1,1 +1,9 @@
-// console.log(map([1, 2, 3, 4, 5], item => item + 10));
+import map from './map';
+
+describe('map', () => {
+  it('should be return after adding 10 each item', () => {
+    const result = map([1, 2, 3, 4, 5], item => item + 10);
+    console.log(result);
+    expect(result).toMatchObject([11, 12, 13, 14, 15]);
+  });
+});
