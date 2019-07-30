dd8d3f18f48c0efe1ac645baeeccc80fce20d0dd
gloriaJun <pureainu@gmail.com>
2019-07-30 23:23:43 +0900

    Add t/c in go.spec.ts

--- src/go.spec.ts
+++ src/go.spec.ts
@@ -1,4 +1,5 @@
 import go from './go';
+import { filter, map, get } from './object';
 
 describe('go', () => {
   it('should be return 4', () => {
@@ -11,5 +12,25 @@ describe('go', () => {
     const result = go(1, a => a + 1, a => a * 2, a => a + 10);
 
     expect(result).toBe(14);
+  });
+
+  it('should be return 14', () => {
+    const users = [
+      { id: 1, name: 'ID', age: 36 },
+      { id: 2, name: 'BJ', age: 16 },
+      { id: 3, name: 'JM', age: 28 },
+      { id: 4, name: 'HA', age: 33 },
+      { id: 5, name: 'JE', age: 18 },
+      { id: 6, name: 'AN', age: 35 },
+      { id: 7, name: 'KA', age: 39 },
+      { id: 8, name: 'WP', age: 31 },
+    ];
+    const result = go(
+      users,
+      filter((user): boolean => user.age > 36),
+      map(get('name')),
+    );
+
+    expect(result).toMatchObject(['KA']);
   });
 });
--- src/object/get.ts
+++ src/object/get.ts
@@ -1,9 +1,11 @@
 import hasKey from './hasKey';
+import { curryRight } from '../curry';
 
 /**
  * @category Object
  * @param {Object} object The object to query.
  * @param {string} key The key to check.
  */
-export default (object: object, key: string): any =>
-  hasKey(object, key) ? object[key] : undefined;
+export default curryRight((object: object, key: string): any =>
+  hasKey(object, key) ? object[key] : undefined,
+);
